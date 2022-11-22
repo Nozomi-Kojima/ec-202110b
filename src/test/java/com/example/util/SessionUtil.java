52fe242b301d737dde28f7330030df8c5d4e6a21
Nozomi Kojima <nozomi.kojima@rakus-partners.co.jp>
2022-11-22 17:56:43 +0900

    11/22 kojima second commit

--- src/test/java/com/example/util/SessionUtil.java
+++ src/test/java/com/example/util/SessionUtil.java
@@ -15,8 +15,8 @@ public class SessionUtil {
 		user.setId(1);
 		user.setName("テストユーザ");
 		user.setEmail("coffeeshop.test@gmail.com");
-		user.setAddress("テスト住所");
 		user.setZipcode("1111111");
+		user.setAddress("テスト住所");
 		user.setTelephone("テスト電話番号");
 		sessionMap.put("userId", user.getId());
 		sessionMap.put("user", user);
