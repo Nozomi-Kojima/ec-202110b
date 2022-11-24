758ee92f3dd0982063dcce9b0b993e513c04f6bc
Nozomi Kojima <nozomi.kojima@rakus-partners.co.jp>
2022-11-24 11:58:49 +0900

    11/24 kojima first commit

--- src/test/java/com/example/controller/MypageControllerTest.java
+++ src/test/java/com/example/controller/MypageControllerTest.java
@@ -3,8 +3,9 @@ package com.example.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import javax.validation.constraints.Email;
+
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -52,11 +53,68 @@ class MypageControllerTest {
 	}
 
 	@Test
-	@DisplayName("マイページ画面への遷移")
+	@DisplayName("マイページ画面へ遷移")
 	void toMypageTest() throws Exception {
 		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();
-		MvcResult mvcResult = mockMvc.perform(get("/toMypage").session(userSession))
+		MvcResult mvcResult = mockMvc.perform(get("/mypage/toMypage").session(userSession))
 				.andExpect(view().name("/mypage/mypage")).andReturn();
 	}
+	@Test
+	@DisplayName("ログインできなかったらログイン画面へ遷移")
+	void toMypageTest2() throws Exception {
+		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();
+		userSession.clearAttributes();
+		MvcResult mvcResult = mockMvc.perform(get("/mypage/toMypage").session(userSession))
+				.andExpect(view().name("forward:/user/toLogin")).andReturn();
+	}
+	@Test
+	@DisplayName("ユーザー更新画面へ遷移")
+	void toUpdateUserTest() throws Exception {
+		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();
+		MvcResult mvcResult = mockMvc.perform(get("/mypage/toUpdateUser").session(userSession))
+				.andExpect(view().name("/mypage/updateUser")).andReturn();
+		userSession.clearAttributes();
+		MvcResult mvcResultfailure = mockMvc.perform(get("/mypage/toUpdateUser").session(userSession))
+				.andExpect(view().name("forward:/user/toLogin")).andReturn();
+		
+	}
+	@Test
+	@DisplayName("ユーザー更新完了画面へ遷移")
+	void toUpdateUserFinishedTest() throws Exception {
+		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();
+		MvcResult mvcResult = mockMvc.perform(get("/mypage/toUpdateUserFinished").session(userSession))
+				.andExpect(view().name("/mypage/updateUserFinished")).andReturn();
+		userSession.clearAttributes();
+		MvcResult mvcResultfailure = mockMvc.perform(get("/mypage/toUpdateUserFinished").session(userSession))
+				.andExpect(view().name("forward:/user/toLogin")).andReturn();
+		
+	}
+	@Test
+	@DisplayName("ユーザー情報削除確認画面へ遷移")
+	void toDeleteUserConfirmTest() throws Exception {
+		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();
+		MvcResult mvcResult = mockMvc.perform(get("/mypage/toDeleteUserConfirm").session(userSession))
+				.andExpect(view().name("/mypage/deleteUserConfirm")).andReturn();
+		userSession.clearAttributes();
+		MvcResult mvcResultfailure = mockMvc.perform(get("/mypage/toDeleteUserConfirm").session(userSession))
+				.andExpect(view().name("forward:/user/toLogin")).andReturn();
+		
+	}
+	@Test
+	@DisplayName("もしユーザー更新結果がエラーだったら")
+	void updateUserConfirmTest() throws Exception {
+		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();		
+		userSession.setNew(false);
+		MvcResult mvcResultError = mockMvc.perform(get("/mypage/updateUserConfirm").session(userSession))
+				.andExpect(view().name("/mypage/updateUser")).andReturn();
+	}
+	@Test
+	@DisplayName("もしメールアドレスが重複していたら")
+	void updateUserConfirmTest2() throws Exception {
+		MockHttpSession userSession = SessionUtil.createUserIdAndUserSession();
+		MvcResult mvcResultDuplication = mockMvc.perform(get("/mypage/updateUserConfirm").session(userSession))
+				.andExpect(view().name("/mypage/updateUser")).andReturn();
+	}
+	
 
 }
--- src/test/java/com/example/util/SessionUtil.java
+++ src/test/java/com/example/util/SessionUtil.java
@@ -1,6 +1,8 @@
 package com.example.util;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.mock.web.MockHttpSession;
@@ -12,14 +14,16 @@ public class SessionUtil {
 	public static MockHttpSession createUserIdAndUserSession() {
 		Map<String, Object> sessionMap = new LinkedHashMap<String, Object>();
 		User user = new User();
+		List<User> userList = new ArrayList<>();
 		user.setId(1);
-		user.setName("テストユーザ");
-		user.setEmail("coffeeshop.test@gmail.com");
-		user.setZipcode("1111111");
+		user.setName("テスト");
+		user.setEmail("test@test.co.jp");
+		user.setZipcode("1112222");
 		user.setAddress("テスト住所");
 		user.setTelephone("テスト電話番号");
+		userList.add(user); 
 		sessionMap.put("userId", user.getId());
-		sessionMap.put("user", user);
+		sessionMap.put("user",userList);
 		return createMockHttpSession(sessionMap);
 	}
 
