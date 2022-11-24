4799dc7ce9e3c85122c97214c4591768783bea88
Tanaka Yuna <yuna.tanaka@rakus-partners.co.jp>
2022-11-24 16:48:16 +0900

    third commit

--- src/test/java/com/example/controller/OrderControllerTest.java
+++ src/test/java/com/example/controller/OrderControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -12,17 +11,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpSession;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+
+@SpringBootTest
 class OrderControllerTest {
-	
+
 	@Autowired
-    private WebApplicationContext wac;
-	
+	private WebApplicationContext wac;
+
 	private MockMvc mockMvc;
 
 	@BeforeAll
@@ -35,7 +36,7 @@ class OrderControllerTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		 mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
 
 	@AfterEach
@@ -44,22 +45,24 @@ class OrderControllerTest {
 
 	@Test
 	@DisplayName("注文確認画面表示")
-	void testtoOrderConfirm() throws Exception{
-		mockMvc.perform(get("/toOrder")
-		).andExpect(view().name("redirect:/login"))
-        .andReturn();
-		
-		mockMvc.perform(post("/register/insert")
+	void testtoOrderConfirm() throws Exception {
+		mockMvc.perform(get("/order/toOrder"))
+		.andExpect(view().name("redirect:/user/toLogin"));
+
+	}
+	@Test
+	void testToOrderConfirm02() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(post("/register/insert")
                 .param("name", "田中佑奈")
                 .param("email", "tanaka@example.com")
                 .param("password", "mogumogu")
                 .param("zipcode", "111-1111")
                 .param("address", "東京都新宿区")
                 .param("telephone", "080-0000-0000")
-        ).andExpect(view().name("/order/order_confirm"))
+        ).andExpect(view().name("order/order_confirm"))
         .andReturn();
-
-		}
+	}
+	
 	
 	
 
