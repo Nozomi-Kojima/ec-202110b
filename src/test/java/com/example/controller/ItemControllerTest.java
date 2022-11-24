773d2bb9a7c6e0d41c9a0a9ebf81b9cc861e5ce3
Kumi Shirai <kumi.shirai@rakus-partners.co.jp>
2022-11-24 15:02:55 +0900

    1124途中更新　Itemｺﾝﾄﾛｰﾗtest

--- src/test/java/com/example/controller/ItemControllerTest.java
+++ src/test/java/com/example/controller/ItemControllerTest.java
@@ -3,14 +3,17 @@ package com.example.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
@@ -21,8 +24,11 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.domain.Item;
+import com.example.form.ItemForm;
+import com.example.form.OrderItemForm;
 import com.example.form.ReviewForm;
 import com.example.util.CsvDataSetLoader;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 
 @SpringBootTest
@@ -52,12 +58,13 @@ class ItemControllerTest {
 		
 		//スコープのデータ取り出し//
 		ModelAndView mav = mvcResult.getModelAndView();
+		@SuppressWarnings("unchecked")
 		List<Item> itemList = (List<Item>)mav.getModel().get("itemList");
 		
 		Item item1 = itemList.get(0);
-		assertEquals("追い鰹チャーシュー", item1.getName());
-		assertEquals(1050, item1.getPriceM());
-		assertEquals(1100, item1.getPriceL());
+		assertEquals("かさね味Special", item1.getName());
+		assertEquals(900, item1.getPriceM());
+		assertEquals(1000, item1.getPriceL());
 	}
 	
 	@Test
@@ -67,7 +74,105 @@ class ItemControllerTest {
 	}
 	
 	@Test
-	void searchItem() throws Exception{
+	@DisplayName("値段が高い順")
+	void searchItem1() throws Exception{
+		MvcResult mvcResult = mockMvc.perform(post("/item/search")
+				.param("sortType","priceHigh"))
+				.andExpect(view().name("/item/item_list_noodle"))
+				.andReturn();
+		
+		ModelAndView mav = mvcResult.getModelAndView();
+//		ItemForm itemForm = new ItemForm("","priceHigh");
+		
+		@SuppressWarnings("unchecked")
+		List<Item> itemList = (List<Item>)mav.getModel().get("itemList");
+		
+		Item item = itemList.get(0);
+		assertEquals("台湾まぜそば", item.getName());
+	}
+	@Test
+	@DisplayName("値段が低い順")
+	void searchItem2() throws Exception{
+		MvcResult mvcResult = mockMvc.perform(post("/item/search")
+				.param("sortType","priceLow"))
+				.andExpect(view().name("/item/item_list_noodle"))
+				.andReturn();
+		
+		ModelAndView mav = mvcResult.getModelAndView();
+		
+		@SuppressWarnings("unchecked")
+		List<Item> itemList = (List<Item>)mav.getModel().get("itemList");
+		
+		Item item = itemList.get(0);
+		assertEquals("旨辛味噌麺", item.getName());
+	}
+	@Test
+	@DisplayName("人気順")
+	@DatabaseSetup("/popularSearch") // テスト実行前に初期データを投入
+	void searchItem3() throws Exception{
+		MvcResult mvcResult = mockMvc.perform(post("/item/search")
+				.param("sortType","popular"))
+				.andExpect(view().name("/item/item_list_noodle"))
+				.andReturn();
+		
+		ModelAndView mav = mvcResult.getModelAndView();
+		
+		@SuppressWarnings("unchecked")
+		List<Item> itemList = (List<Item>)mav.getModel().get("itemList");
+		
+		Item item = itemList.get(0);
+		assertEquals("赤ラーメン", item.getName());
+		Item item2 = itemList.get(1);
+		assertEquals("追い鰹チャーシュー", item2.getName());
+	}
+	@Test
+	@DisplayName("名前検索　人気順")
+	void searchItem4() throws Exception{
+		MvcResult mvcResult = mockMvc.perform(post("/item/search")
+				.param("name","百").param("sortType", "popular"))
+				.andExpect(view().name("/item/item_list_noodle"))
+				.andReturn();
+		
+		ModelAndView mav = mvcResult.getModelAndView();
+		
+		@SuppressWarnings("unchecked")
+		List<Item> itemList = (List<Item>)mav.getModel().get("itemList");
+		
+		Item item = itemList.get(0);
+		assertEquals("百福元味", item.getName());
+		}
+	@Test
+	@DisplayName("名前検索　該当なし")
+	void searchItem5() throws Exception{
+		MvcResult mvcResult = mockMvc.perform(post("/item/search")
+				.param("name","v").param("sortType", "popular"))
+				.andExpect(view().name("/item/item_list_noodle"))
+				.andReturn();
+		
+		ModelAndView mav = mvcResult.getModelAndView();
+		
+		String message = (String)mav.getModel().get("message");
+		
+		assertEquals("該当の商品が見つかりません", message);
+	}
+	@Test
+	void setUpOrderItemForm() throws Exception{
+		OrderItemForm form = itemController.setUpOrderItemForm();
+		assertTrue(form instanceof OrderItemForm);
+	}
+	
+	@Test
+	void showDetail() throws Exception{
+		MvcResult mvcResult = mockMvc.perform(post("/item/showDetail")
+				.param("id","4"))
+				.andExpect(view().name("/item/item_detail"))
+				.andReturn();
+		
+		ModelAndView mav = mvcResult.getModelAndView();
+		
+		Item item = (Item)mav.getModel().get("item");
+		
+		assertEquals("かさね味Special", item.getName());
 		
 	}
 
--- src/test/java/com/example/util/SessionUtil.java
+++ src/test/java/com/example/util/SessionUtil.java
@@ -1,6 +1,8 @@
 package com.example.util;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.mock.web.MockHttpSession;
@@ -12,14 +14,17 @@ public class SessionUtil {
 	public static MockHttpSession createUserIdAndUserSession() {
 		Map<String, Object> sessionMap = new LinkedHashMap<String, Object>();
 		User user = new User();
+		List<User> userList = new ArrayList<>();
 		user.setId(1);
 		user.setName("テストユーザ");
 		user.setEmail("@gmail.com");
-		user.setAddress("テスト住所");
+		user.setPassword("Abcd1234");
 		user.setZipcode("1111111");
+		user.setAddress("テスト住所");
 		user.setTelephone("テスト電話番号");
+		user.setPasswordConfirm("Abcd1234");
 		sessionMap.put("userId", user.getId());
-		sessionMap.put("user", user);
+		sessionMap.put("user", userList);
 		return createMockHttpSession(sessionMap);
 	}
 
